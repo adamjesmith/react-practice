8089c9c6ed43d827f38ae0879a3e6a4d27ea8ec4
adam-wit <adam@wit-fitness.com>
2021-03-11 20:15:18 +0000

    Installed and configured babel

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -29846,7 +29846,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64934" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51200" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
